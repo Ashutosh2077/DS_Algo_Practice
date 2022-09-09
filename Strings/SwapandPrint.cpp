bcc12b113d0fc20e4ef239650b6eca8b9b78e4c8
Ashutosh Singh <1901330310038@niet.co.in>
2022-09-10 00:35:11 +0530

    Updated Swaping code

--- Strings/SwapandPrint.cpp
+++ Strings/SwapandPrint.cpp
@@ -1,10 +1,30 @@
 #include <iostream>
 using namespace std;
+void swapTwoNumbers(int a, int b)
+{
+    // // swap by using swap function
+
+    // swap(a, b);
+
+    // // swap by using Extra Temp Variable
+    // int temp;
+    // temp = a;
+    // a = b;
+    // b = temp;
+
+    // // Swap by additon
+
+    a = a + b;
+    b = a - b;
+    a = a - b;
+
+    cout << "a = " << a << " b = " << b << endl;
+}
 int main()
 {
     // Enter Code Here
     string str;
-    cout << "enter the string";
+    cout << "enter the string" << endl;
     cin >> str;
     int s = 0;
     int e = str.length() - 1;
@@ -20,4 +40,9 @@ int main()
         str[e] = temp;
         cout << str << endl;
     }
+
+    cout << "Swap Two Number enter Two Numbers" << endl;
+    int a, b;
+    cin >> a >> b;
+    swapTwoNumbers(a, b);
 }
